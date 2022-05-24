3fcbcf0475e949a9302bc47c9120640621db2c07
Artur Bruno <artbruno35@gmail.com>
2022-05-24 11:01:54 -0300

    update settings

--- config/settings.py
+++ config/settings.py
@@ -13,13 +13,17 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*k9akia426zsc8uf(dt6yb%oj5h(&(11&89d6tau8iugth=7dt'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+django_heroku.settings(locals())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
